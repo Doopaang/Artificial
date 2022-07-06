f3a135518728ff3f0a5ff497d19c61d9b2d724ee
KimJuYeong <43029921+dafher801@users.noreply.github.com>
2022-07-06 12:13:10 +0900

    인벤토리, 오브젝트
    오브젝트 상호작용
    인벤토리 슬롯 구현 중

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    public Image slotImage;
+
+    public Image itemImage;
+
+    ItemData item;
+
+    private void Start()
+    {
+
+    }
+
+    public void ChangeItem()
+    {
+
+    }
+}

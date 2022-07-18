9ba1edf282aa14a2bdeca13a549878c52f9691a7
KimJuYeong <43029921+dafher801@users.noreply.github.com>
2022-07-18 18:24:48 +0900

    자물쇠 작업

--- Assets/Scripts/Object/InteractiveObject/Chapter1/Clock.cs
+++ Assets/Scripts/Object/InteractiveObject/Chapter1/Clock.cs
@@ -4,6 +4,11 @@ public class Clock : MonoBehaviour
 {
     public void Interact()
     {
+        DialogueSystem.Instance.StartDialogue("ClockInteract", GainClock);
+    }
+
+    public void GainClock()
+    {
         GameManager.Instance.inventory.GainItem(EItemType.CHAPTER1_EMPTY_WATCH);
         gameObject.SetActive(false);
     }
